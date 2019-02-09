a7408c47d1af069a9c6c50ce3f426a85f55483fd
Mehdi <mshaffaf@gmail.com>
2019-02-08 23:26:54 -0500

    Add project files.

---
+++ FluentAPI/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using DataAnnotations;
+
+namespace FluentAPI.EntityConfigurations
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        public CourseConfiguration()
+        {
+            ToTable("tbl-Course");
+            HasKey(c => c.Id);
+
+            Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(255);
+
+            Property(c => c.Description)
+            .IsRequired()
+            .HasMaxLength(2000);
+
+            HasRequired(c => c.Author)
+            .WithMany(a => a.Courses)
+            .HasForeignKey(c => c.AuthorId)
+            .WillCascadeOnDelete(false);
+
+            HasRequired(c => c.Cover)
+           .WithRequiredPrincipal(c => c.Course);
+
+            HasMany(c => c.Tags)
+            .WithMany(t => t.Courses)
+            .Map(m =>
+            {
+                m.ToTable("CourseTags");
+                m.MapLeftKey("CourseId");
+                m.MapRightKey("TagId");
+            });
+
+
+        }
+
+    }
+}
